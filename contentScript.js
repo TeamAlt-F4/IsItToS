2d4f164588e87a919df58261c35f12ec5f9fe009
JonathanM11 <Jonathan.manda11@gmail.com>
2022-04-06 17:45:36 -0500

    update

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,9 @@
-var playlistLength;
+var playlistLength = 7;
 var position = 130;
 var dots = {
-    "circle": [],
-    "data": []
+    "circle" : [],
+    "data" : []
 }
-
-const vidIDs = [];
 /*
 const SAFE_VIDEO = "SAFE_STATE";
 const QUESTIONABLE_VIDEO = "QUESTIONABLE_STATE";
@@ -65,44 +63,25 @@ function isModRestricted() { //fetch mod data
 }
 */
 
-window.addEventListener('load', (event) => {
-    getVideoIDs();
-    dotConstuctor();
-    console.log(vidIDs);
-    getVideoStatus();
-});
+dotConstuctor();
 
-let result = document.getElementById;
-console.log(result);
+    
 
 function dotConstuctor() {
-    for (var i = 0; i < playlistLength; i++) {
-        dots.circle[i] = document.createElement('div');
+    for(var i = 0; i < playlistLength; i++) {
+        dots.circle[i] = document.createElement('div'); // create dots
         dots.circle[i].className = 'dot';
-        dots.circle[i].setAttribute("id", i);
+        dots.circle[i].setAttribute("id", i);   
         document.body.appendChild(dots.circle[i]);
         var elementStyle = document.getElementById(i).style;
         elementStyle.top = position + 'px';
         position = position + 101;
-        hover(dots.data[i]);
         dots.circle[i].classList.add('green');  //temp line
-    }
-}
-
-function getVideoIDs() {
-    var links = document.querySelectorAll('#video-title');
-
-    for (var i = 0; i < links.length; i++) {
-        vidIDs.push(links[i].href.slice(32, 43));
-    }
-    playlistLength = vidIDs.length;
-}
-
-function getVideoStatus() {
-    for (var i = 0; i < vidIDs.length; i++) {
-        fetch("http://localhost:3000/getStatus/" + vidIDs[i], {mode: 'no-cors'}).then(data => {
-            console.log('Success:', data)
-        });
+        
+        hover(i);
+        
+        
+        
     }
 }
 
@@ -114,9 +93,21 @@ function setDotColor(dot) {
     } else dot.classList.add('yellow');
 }
 
-function hover(dot) {
-    dot = document.createElement('div');
-    dot.classList.add('hoverData');
-    dot.addEventListener('mouseenter', () => dot.zIndex = 9);
-    dot.addEventListener('mouseleave', () => dot.zIndex = 0);
+function hover(i) {    
+    dots.data[i] = document.createElement('div');
+    dots.data[i].className = 'hoverData';
+    dots.data[i].setAttribute("id", i + 100);
+    dots.circle[i].appendChild(dots.data[i]);
+    let dataStyle = document.getElementById(i + 100).style; 
+        
+    dots.circle[i].addEventListener('mouseenter', () => {
+        dataStyle.backgroundColor = 'white';
+        dataStyle.color = 'black';
+    });
+    
+    dots.circle[i].addEventListener('mouseleave', () => {
+        dataStyle.backgroundColor = 'transparent';
+        dataStyle.color = 'transparent';
+    });
+    
 }
