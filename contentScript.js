ef4c50df9dd8e722296cf182636c256969bdf39f
John Meyers <john@johnmeye.rs>
2022-04-06 17:56:47 -0500

    deleted obsolete code

--- contentScript.js
+++ contentScript.js
@@ -5,64 +5,6 @@ var dots = {
     "data" : []
 }
 const vidIDs = [];
-/*
-const SAFE_VIDEO = "SAFE_STATE";
-const QUESTIONABLE_VIDEO = "QUESTIONABLE_STATE";
-const UNKNOWN_VIDEO = "UNKNOWN_STATE";
-const DANGEROUS_VIDEO = "DANGEROUS_STATE"
-
-
-//finds the thumbnail on the page and returns the location to add our icon
-function getThumbnail() {
-    if (document.getElementsById("thumbnail")?.offsetParent === null) {
-        return document.querySelector("ytd-menu-thumbnail-content > div");
-    }
-}
-
-//api request to our server to get data
-fetch(
-    `https://isittos.gg/videoDatabase?videoId=${getVideoID()}`
-)
-
-
-function addOverlay(){
-    //css code and logic to add and overlay
-}
-
-
-function getState() {
-    if (isVideoSafe()) {
-        return SAFE_VIDEO;
-    }
-    if (!isVideoSafe()) {
-        return DANGEROUS_VIDEO;
-    } else {
-        return NEUTRAL_STATE;
-    }
-
-}
-
-function getVideoID(){
-    return videoID;
-}
-
-function isVideoSafe(){
-    if (current.video == ageRestricted){
-        return False;
-    } if (current.video == isModRestricted) {
-        return False;
-    } else {
-        return True;
-    }
-}
-
-function isModRestricted() { //fetch mod data
-    if (current.video == modRestricted) {
-        return False
-    } else return True;
-
-}
-*/
 
 window.addEventListener('load', (event) => {
     getVideoIDs();
