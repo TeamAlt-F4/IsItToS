3b3ae98df4ffe59264c322dda0a206cecf64243d
EJMani <98973368+EJMani@users.noreply.github.com>
2022-04-07 13:20:24 -0500

    fixed return issue

--- server.js
+++ server.js
@@ -2,30 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const {google, datacatalog_v1} = require('googleapis');
-const XLSX = require('xlsx');
+const XLSX = require('xlsx')
 
 //------------------Searches Database and Youtube API for rating------------------//
 app.get('/getStatus/:VideoID', (req, res) =>{
     
   //localhost:3000/getStatus/{VideoID}        <-- Example call from localhost
     
-var VideoID = req.params.VideoID;
-var wb = XLSX.readFile('./database.xlsx');
-var ws = wb.Sheets['Sheet1'];
-var data = XLSX.utils.sheet_to_json(ws);
-var notFound = new Boolean(true);
+  var VideoID = req.params.VideoID;
+  var wb = XLSX.readFile('./database.xlsx');
+  var ws = wb.Sheets['Sheet1'];
+  var data = XLSX.utils.sheet_to_json(ws);
+  var notFound = new Boolean(true);
 
-//Search Database
-for(var i= 0;i<data.length;i++){
-  if(data[i].video_id == VideoID){
-    console.log(data[i].age_restricted);
-    res.send(data[i].age_restricted.toString());
-    notFound = false;
-    break;
+  //Search Database
+  for(var i= 0;i<data.length;i++){
+    if(data[i].video_id == VideoID){
+      const Response = '{"moderator rating":"'+data[i].moderator_rating+'", "comment":"'+data[i].comment+'"}'
+      const JSONResponse = JSON.parse(Response);
+      console.log(JSONResponse);
+      res.send(JSONResponse);
+      notFound = false;
+      break;
+    }
   }
-}
 
-  //If not found in database, search from YouTube API
+  //If not found in database, search from Youtube API
   if(notFound){
     google.youtube('v3').videos.list({
       key: 'AIzaSyAk7sGBtLkbPyUuq-i9KlJsB_uQWufpv08',
@@ -35,17 +37,22 @@ for(var i= 0;i<data.length;i++){
     }).then((Response) =>{
       
       //console.log(JSON.stringify(Response.data.items[0].contentDetails.contentRating));
-      var ageRestricted = JSON.stringify(Response.data.items[0].contentDetails.contentRating);
+      var ageRestricted = JSON.stringify(Response.data.items[0].contentDetails.contentRating.ytRating);
       
       //Set rating
-      if(ageRestricted = " "){
-        res.send('0');
+      var Response;
+      if(ageRestricted == "ytAgeRestricted"){
+        const Response = '{"moderator rating":"nsfs", "comment":" "}'
+        const JSONResponse = JSON.parse(Response);
+        res.send(JSONResponse);
+        var rating = 1;
+        var MR = 'nsfs'
+      }else{
+        const Response = '{"moderator rating":"safe", "comment":" "}'
+        const JSONResponse = JSON.parse(Response);
+        res.send(JSONResponse);
         var rating = 0;
         var MR = 'safe';
-      }else{
-        res.send('1');
-        var rating = 1;
-        var MR = 'not safe'
       }
 
       //New entry
@@ -57,7 +64,7 @@ for(var i= 0;i<data.length;i++){
       }]
 
       //Add new Video ID to Database with rating
-      var size = data.length+2;``
+      var size = data.length+2;
       XLSX.utils.sheet_add_json(ws, input, {skipHeader:true, origin: 'A'+size});
       XLSX.writeFile(wb,'./database.xlsx');
       console.log('Added to Database');
@@ -68,8 +75,20 @@ for(var i= 0;i<data.length;i++){
 });
 //--------------------------------------------------------------------------------//
 
-app.get('/Comment',(req,res) =>{
-  res.send('hi');
+app.get('/getComment/:VideoID',(req,res) =>{
+  
+  var VideoID = req.params.VideoID;
+  var wb = XLSX.readFile('./database.xlsx');
+  var ws = wb.Sheets['Sheet1'];
+  var data = XLSX.utils.sheet_to_json(ws);
+
+  //Search Database
+  for(var i= 0;i<data.length;i++){
+    if(data[i].video_id == VideoID){
+      res.send(data[i].comments);
+      break;
+    }
+  }
 });
 
 app.listen(port, () => {
